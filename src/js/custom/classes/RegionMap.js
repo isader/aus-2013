bf63090f4e13139cc11536b22962186695afdb73
Imad Sader <imadsader@gmail.com>
2013-08-01 15:48:42 +1000

    Converting map points to GeoJSON

--- src/js/custom/classes/RegionMap.js
+++ src/js/custom/classes/RegionMap.js
@@ -89,14 +89,15 @@ RegionMap.prototype.buildMap = function(id) {
 		name : "Aus Map Styles"
 	});
 	//
-	this.centerLatlng = new google.maps.LatLng(-20.591652120829167, 145.986328125);
-	this.orgZoom = 5;
+	this.centerLatlng = new google.maps.LatLng(-25.274398, 133.775136);
+	this.orgZoom = 4;
 	var myOptions = {
 		zoom : this.orgZoom,
 		minZoom : this.orgZoom,
-		panControl : true,
+		panControl : false,
 		scaleControl : false,
 		zoomControl : true,
+		mapTypeControl: false,
 		streetViewControl : false,
 		center : this.centerLatlng,
 		mapTypeIds : [google.maps.MapTypeId.ROADMAP, 'aus_styles']
@@ -106,6 +107,242 @@ RegionMap.prototype.buildMap = function(id) {
 	this.map = new google.maps.Map(document.getElementById(id), myOptions);
 	this.map.mapTypes.set('aus_styles', ausMapStyles);
 	this.map.setMapTypeId('aus_styles');
+
+
+
+
+	var _geometryToGoogleMaps = function( geojsonGeometry, opts, geojsonProperties ){
+		
+		var googleObj,
+			bounds = new google.maps.LatLngBounds();;
+		
+		switch ( geojsonGeometry.type ){
+			case "Polygon":
+				var paths = [];
+				var exteriorDirection;
+				var interiorDirection;
+				for (var i = 0; i < geojsonGeometry.coordinates.length; i++){
+					var path = [];
+					for (var j = 0; j < geojsonGeometry.coordinates[i].length; j++){
+						var ll = new google.maps.LatLng(geojsonGeometry.coordinates[i][j][1], geojsonGeometry.coordinates[i][j][0]);
+						path.push(ll);
+					}
+					if(!i){
+						exteriorDirection = _ccw(path);
+						paths.push(path);
+					}else if(i == 1){
+						interiorDirection = _ccw(path);
+						if(exteriorDirection == interiorDirection){
+							paths.push(path.reverse());
+						}else{
+							paths.push(path);
+						}
+					}else{
+						if(exteriorDirection == interiorDirection){
+							paths.push(path.reverse());
+						}else{
+							paths.push(path);
+						}
+					}
+				}
+				opts.paths = paths;
+				googleObj = new google.maps.Polygon(opts);
+				googleObj.setMap(selfRef.map);
+				if (geojsonProperties) {
+					googleObj.set("geojsonProperties", geojsonProperties);
+				}
+				break;
+				
+			case "MultiPolygon":
+				googleObj = [];
+				for (var i = 0; i < geojsonGeometry.coordinates.length; i++){
+					var paths = [];
+					var exteriorDirection;
+					var interiorDirection;
+					for (var j = 0; j < geojsonGeometry.coordinates[i].length; j++){
+						var path = [];
+						for (var k = 0; k < geojsonGeometry.coordinates[i][j].length; k++){
+							var ll = new google.maps.LatLng(geojsonGeometry.coordinates[i][j][k][1], geojsonGeometry.coordinates[i][j][k][0]);
+							path.push(ll);
+						}
+						if(!j){
+							exteriorDirection = _ccw(path);
+							paths.push(path);
+						}else if(j == 1){
+							interiorDirection = _ccw(path);
+							if(exteriorDirection == interiorDirection){
+								paths.push(path.reverse());
+							}else{
+								paths.push(path);
+							}
+						}else{
+							if(exteriorDirection == interiorDirection){
+								paths.push(path.reverse());
+							}else{
+								paths.push(path);
+							}
+						}
+					}
+					opts.paths = paths;
+					googleObj.push(new google.maps.Polygon(opts));
+				}
+				if (geojsonProperties) {
+					for (var k = 0; k < googleObj.length; k++){
+						//console.log(geojsonProperties);
+						googleObj[k].setMap(selfRef.map);
+						googleObj[k].set("geojsonProperties", geojsonProperties);
+					}
+				}
+				break;
+				
+			default:
+				googleObj = _error("Invalid GeoJSON object: Geometry object must be one of \"Point\", \"LineString\", \"Polygon\" or \"MultiPolygon\".");
+		}
+		
+		return googleObj;
+		
+	};
+	
+	var _error = function( message ){
+	
+		return {
+			type: "Error",
+			message: message
+		};
+	
+	};
+
+	var _ccw = function( path ){
+		var isCCW;
+		var a = 0;
+		for (var i = 0; i < path.length-2; i++){
+			a += ((path[i+1].lat() - path[i].lat()) * (path[i+2].lng() - path[i].lng()) - (path[i+2].lat() - path[i].lat()) * (path[i+1].lng() - path[i].lng()));
+		}
+		if(a > 0){
+			isCCW = true;
+		}
+		else{
+			isCCW = false;
+		}
+		return isCCW;
+	};
+
+
+	var geojson = mapRegions;
+	var obj = [];
+	var opts = {
+		strokeColor : "#FFFFFF",
+		strokeOpacity : 0.7,
+		strokeWeight : 1,
+		fillColor : "#000000",
+		fillOpacity : 0.6
+	};
+
+
+//_geometryToGoogleMaps(geojson.features[i].geometry, opts, geojson.features[i].properties);
+	for (var i = 0; i < geojson.features.length; i++) {
+		var geojsonGeometry = geojson.features[i].geometry;
+		var geojsonProperties = geojson.features[i].properties;
+		var regionName = geojson.features[i].properties.ELECT_DIV;
+		var electorateData = dataInterface.findElectorateData(regionName.toLowerCase());
+		var bounds = new google.maps.LatLngBounds();
+		var googleObj;
+
+		(function () {
+		switch ( geojsonGeometry.type ){
+			case "Polygon":
+				var paths = [];
+				var exteriorDirection;
+				var interiorDirection;
+				for (var i = 0; i < geojsonGeometry.coordinates.length; i++){
+					var path = [];
+					for (var j = 0; j < geojsonGeometry.coordinates[i].length; j++){
+						var ll = new google.maps.LatLng(geojsonGeometry.coordinates[i][j][1], geojsonGeometry.coordinates[i][j][0]);
+						//bounds.extend(ll);
+						path.push(ll);
+					}
+					if(!i){
+						exteriorDirection = _ccw(path);
+						paths.push(path);
+					}else if(i == 1){
+						interiorDirection = _ccw(path);
+						if(exteriorDirection == interiorDirection){
+							paths.push(path.reverse());
+						}else{
+							paths.push(path);
+						}
+					}else{
+						if(exteriorDirection == interiorDirection){
+							paths.push(path.reverse());
+						}else{
+							paths.push(path);
+						}
+					}
+				}
+				opts.paths = paths;
+				googleObj = new google.maps.Polygon(opts);
+				//console.log(geojsonProperties);
+				googleObj.setMap(selfRef.map);
+				this.districtPolygons[(regionName.toLowerCase()).replace('\'', '')] = googleObj;
+				if (geojsonProperties) {
+					googleObj.set("geojsonProperties", geojsonProperties);
+				}
+				break;
+				
+			case "MultiPolygon":
+				googleObj = [];
+				for (var i = 0; i < geojsonGeometry.coordinates.length; i++){
+					var paths = [];
+					var exteriorDirection;
+					var interiorDirection;
+					for (var j = 0; j < geojsonGeometry.coordinates[i].length; j++){
+						var path = [];
+						for (var k = 0; k < geojsonGeometry.coordinates[i][j].length; k++){
+							var ll = new google.maps.LatLng(geojsonGeometry.coordinates[i][j][k][1], geojsonGeometry.coordinates[i][j][k][0]);
+							bounds.extend(ll);
+							path.push(ll);
+						}
+						if(!j){
+							exteriorDirection = _ccw(path);
+							paths.push(path);
+						}else if(j == 1){
+							interiorDirection = _ccw(path);
+							if(exteriorDirection == interiorDirection){
+								paths.push(path.reverse());
+							}else{
+								paths.push(path);
+							}
+						}else{
+							if(exteriorDirection == interiorDirection){
+								paths.push(path.reverse());
+							}else{
+								paths.push(path);
+							}
+						}
+					}
+					opts.paths = paths;
+					googleObj.push(new google.maps.Polygon(opts));
+				}
+				if (geojsonProperties) {
+					for (var k = 0; k < googleObj.length; k++){
+						//console.log(geojsonProperties);
+						googleObj[k].setMap(selfRef.map);
+						googleObj[k].set("geojsonProperties", geojsonProperties);
+					}
+				}
+				break;
+				
+			default:
+				googleObj = _error("Invalid GeoJSON object: Geometry object must be one of \"Point\", \"LineString\", \"Polygon\" or \"MultiPolygon\".");
+		}
+		})();
+	}
+
+
+console.log(this.districtPolygons);
+
+
+
 
 	this.mapRegions = new MapRegions();
 	var regionsList = this.mapRegions.getRegions();
@@ -134,7 +371,7 @@ RegionMap.prototype.buildMap = function(id) {
 			id : i
 			
 		});
-		this.districtPolygons[regionsList[i].name.toLowerCase()] = regionPolygon;
+		//this.districtPolygons[regionsList[i].name.toLowerCase()] = regionPolygon;
 
 		// add marker
 		var marker;
