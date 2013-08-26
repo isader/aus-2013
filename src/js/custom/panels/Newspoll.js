fbf25f7d3deab1354b82efd0c5956b025a313f2c
Imad Sader <imadsader@gmail.com>
2013-08-26 12:26:51 +1000

    updated newspoll feeds

--- src/js/custom/panels/Newspoll.js
+++ src/js/custom/panels/Newspoll.js
@@ -265,8 +265,8 @@ NewsPoll.prototype.buildBetterPM = function() {
         xAxis: {
             type: 'datetime',
         	tickInterval: 86400000,
-        	min: 1372636800000 - (86400000 * 3),
-        	max: 1376870400000 + (86400000 * 5),
+        	min: 1373932800000 - (86400000 * 3),
+        	max: 1377475200000 + (86400000 * 5),
 			labels: {
 			    align : 'center',
 			    x: 0, y: 25,
